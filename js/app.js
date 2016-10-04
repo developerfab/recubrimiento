ac0c3a52b94c9f51b781096127d4502db7d1a2fd
Sergio Gutiérrez <sergut18@gmail.com>
2016-10-03 19:18:46 -0500

    fixed eliminar elementales

--- js/app.js
+++ js/app.js
@@ -22,6 +22,10 @@ var l0   ="";
 var luno = "";
 var ldos = "";
 
+var atr;
+var DF;
+var elementales;
+var isValid;
 //CARGA DEL TEXTO INGRESADO
 function load(){
   $("#alert").text("");
@@ -30,53 +34,54 @@ function load(){
   $("#l2").text("");
   strT            = $("#conjunto").val();
   strDF           = $("#df").val();
-  var atr = atributos(strT);
-  var DF          = strDF.match(regexpDF);
-  var elementales = comprobarElementales(DF);
-  var isValid     = comprobarDF(DF, atributos(strT));
+  atr = atributos(strT);
+  DF          = strDF.match(regexpDF);
+  elementales = comprobarElementales(DF);
+  isValid     = comprobarDF(DF, atributos(strT));
   if (isValid.length > 0){
     $("#alert1-text").text("{ " + isValid + " }");
     error1.open();
   }else if(elementales.length > 0){
     $("#alert-text").text("{ " + elementales+ " }");    
     error2.open();
-    strDF = eliminarElementales(DF, elementales);
+    strDF = eliminarElementales(DF, elementales);    
     console.log(DF);
-    $("#df").text(DF);
-  }else {
-    l0   = lcero(DF);
-    luno = l1(l0);
-    ldos = l2(luno, luno);
-    var pl0   = prepare(l0);
-    var pluno = prepare(luno);
-    var pldos = prepare(ldos);  
-    $("#l0").text("{ "+pl0+" }") 
-    $("#l1").text("{ "+pluno+" }") 
-    $("#l2").text("{ "+pldos.toString()+" }") 
-    var w = getW(ldos, atr);
-    var z = getZ(ldos, atr);
-    var v = getV(z, w, atr);
-    $("#z").text("Z = { "+atr+" }" + " - " + "{ "+ getImplicados(ldos) +" } = { " + z + " }");
-    $("#zp").text("Z+ = { "+ redundancia(z, ldos) +" }");
-    
-
-    if (probarZ(ldos, atr)){
-      $(".llaves").addClass("hiden");
-      $("#resz").text("Z es igual a T, entonces Z es llave primaria.");
-    }else {
-      $(".llaves").removeClass("hiden");
-      $("#resz").text("Z es diferente a T");
-      $("#w").text("W = { "+atr+" }" + " - " + "{ "+ getImplicantes(ldos) +" } = { " + w + " }");
-      $("#wp").text("W+ = { "+ redundancia(w, ldos) +" }");
-
-      $("#v").text("V = { " + v + " }");
-      var M2_all = getM2(z,v,ldos,atr);
-      var M2 = getM2Optimo(M2_all);
-      var pM2 = prepare(M2);  
-      $("#llaves").text("Llaves primarias = { "+ pM2.toString() +" }");
-    }
+    DF = strDF;
+    $("#df").val(DF);
 
   }
+  l0   = lcero(DF);
+  luno = l1(l0);
+  ldos = l2(luno, luno);
+  var pl0   = prepare(l0);
+  var pluno = prepare(luno);
+  var pldos = prepare(ldos);  
+  $("#l0").text("{ "+pl0+" }") 
+  $("#l1").text("{ "+pluno+" }") 
+  $("#l2").text("{ "+pldos.toString()+" }") 
+  var w = getW(ldos, atr);
+  var z = getZ(ldos, atr);
+  var v = getV(z, w, atr);
+  $("#z").text("Z = { "+atr+" }" + " - " + "{ "+ getImplicados(ldos) +" } = { " + z + " }");
+  $("#zp").text("Z+ = { "+ redundancia(z, ldos) +" }");
+  
+
+  if (probarZ(ldos, atr)){
+    $(".llaves").addClass("hiden");
+    $("#resz").text("Z es igual a T, entonces Z es llave primaria.");
+  }else {
+    $(".llaves").removeClass("hiden");
+    $("#resz").text("Z es diferente a T");
+    $("#w").text("W = { "+atr+" }" + " - " + "{ "+ getImplicantes(ldos) +" } = { " + w + " }");
+    $("#wp").text("W+ = { "+ redundancia(w, ldos) +" }");
+
+    $("#v").text("V = { " + v + " }");
+    var M2_all = getM2(z,v,ldos,atr);
+    var M2 = getM2Optimo(M2_all);
+    var pM2 = prepare(M2);  
+    $("#llaves").text("Llaves primarias = { "+ pM2.toString() +" }");
+  }
+
 }
 
 function eliminarElementales(DF, elementales){
